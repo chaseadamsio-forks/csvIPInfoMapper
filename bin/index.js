ed4da68ccfe8d0ffdf86d3f161aa474812ff2e30
n4sa <jacobtipp@gmail.com>
2015-07-16 16:56:14 -0700

    updated readme, added new command-line argument <field>

--- bin/index.js
+++ bin/index.js
@@ -6,13 +6,21 @@ var Program = require('../lib/program');
 
 commander
   .version(pkgvrsn)
+  .option('-k, --field [field]', 'name of ip address field')
   .option('-f, --file [fileName]', 'CSV file containing ip addresses')
   .parse(process.argv);
 
 if (!commander.file) {
-  throw Error('Please pass in a filename containing ip address');
+  console.error('Please pass in a filename containing ip address -f <fileName>')
+  process.exit(1)
+}
+
+if(!commander.field) {
+  console.error('Please provide a fieldname -k <fieldname>')
+  process.exit(1)
 }
 
 new Program({
-  filePath: process.cwd() + '/' + commander.file
+  filePath: process.cwd() + '/' + commander.file,
+  field: commander.field
 });
